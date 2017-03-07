b38e8efc36bdba1b4a8b0c37d92001d7b0e2a0ce
Miguel <Miguel@DESKTOP-D7LBKUP>
2017-03-06 21:00:51 -0300

    Manejo de Sesiones

--- SistemaCalculoweb/Controllers/UsuariosController.cs
+++ SistemaCalculoweb/Controllers/UsuariosController.cs
@@ -38,21 +38,27 @@ namespace SistemaCalculoweb.Controllers
             }
             
         }
+        public ActionResult LogOff()
+        {
+            Session["Usuario"] = null;
+            return RedirectToAction("Index", "Home");
+        }
         public ActionResult Login(Usuarios usuarios)
         {
             if (usuarios.Correo != null && usuarios.Correo != "" && usuarios.Contrasenia != null && usuarios.Contrasenia != "")
             {
-                List<Usuarios> us = db.Usuarios.Where(i => i.Correo == usuarios.Correo && usuarios.Contrasenia==i.Contrasenia).ToList();
-                GMailer.GmailUsername = "miguelrvl@gmail.com";
-                GMailer.GmailPassword = "lenoxmrv";
-
-                GMailer mailer = new GMailer();
-                mailer.ToEmail = us[0].Correo.ToString();
-                mailer.Subject = "Recuperacion de contraseña";
-                mailer.Body = "Su contraseña es <br>" + us[0].Contrasenia.ToString();
-                mailer.IsHtml = true;
-                mailer.Send();
-                return RedirectToAction("Index", "Home");
+                List<Usuarios> us = db.Usuarios.Where(i => i.Correo == usuarios.Correo && usuarios.Contrasenia == i.Contrasenia).ToList();
+                if (us.Count > 0)
+               {
+                    Session["Usuario"] = us[0].Nombre + " " + us[0].ApellidoP;
+                    return RedirectToAction("Index", "Home");
+                }
+               else
+               {
+                    ViewBag.Error = "nombre de usuario o contraseña incorrecta";
+                    return View();
+                }
+                 
             }
             else
             {
