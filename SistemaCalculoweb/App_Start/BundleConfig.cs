dbbe7b125aef3c5c41c1d6b51814aea81649dba9
Miguel <Miguel@DESKTOP-D7LBKUP>
2017-03-07 23:17:36 -0300

    content

--- SistemaCalculoweb/App_Start/BundleConfig.cs
+++ SistemaCalculoweb/App_Start/BundleConfig.cs
@@ -24,8 +24,8 @@ namespace SistemaCalculoweb
                       "~/Scripts1/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content1/bootstrap.css",
+                      "~/Content1/site.css"));
         }
     }
 }
