bd9e1395ab1f12a1732a01ceb4fb725b84fb9d50
Miguel Reyes <mreyes@saydex.cl>
2017-03-14 13:26:02 -0300

    nueva vista

---
+++ SistemaCalculoweb/Controllers/NesterGridCalculoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaCalculoweb.Controllers
+{
+    public class NesterGridCalculoController : Controller
+    {
+        // GET: NesterGridCalculo
+        public ActionResult List()
+        {
+            List<VistaVM> allOrder = new List<VistaVM>();
+
+            // here MyDatabaseEntities is our data context
+            
+            using (CalculoEntities dc = new CalculoEntities())
+            {
+                var o = dc.Calculo.OrderByDescending(a => a.Id_calculo);
+                foreach (var i in o)
+                {
+                    var od = dc.CalculoHoras.Where(a => a.Id_calculo==i.Id_calculo).ToList();
+                    allOrder.Add(new VistaVM { calculoPrincipal = i, calculoDetalle = od });
+                }
+            }
+            return View(allOrder);
+        }
+    }
+}
---
+++ SistemaCalculoweb/VistaVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCalculoweb
+{
+   public class VistaVM
+    {
+        public Calculo calculoPrincipal { get; set; }
+        public List<CalculoHoras> calculoDetalle { get; set; }
+    }
+}
